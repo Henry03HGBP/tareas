805230cd264a6d35d30aa3f4e3ed4ed3a733e2ca
Henry03HGBP <henry.bautista.prado@icloud.com>
2023-10-02 22:25:26 -0600

    backend terminado

--- backend/controllers/tareasControllers.js
+++ backend/controllers/tareasControllers.js
@@ -1,7 +1,10 @@
 const asyncHandler = require("express-async-handler")
+const Tarea = require("../models/tareaModel")
 
 const getTareas = asyncHandler( async (req, res) => {
-    res.status(200).json({message: 'Obtener las tareas'})
+    const tareas = await Tarea.find()
+
+    res.status(200).json({tareas})
 })
 
 const setTarea = asyncHandler( async (req, res) => {
@@ -16,11 +19,29 @@ const setTarea = asyncHandler( async (req, res) => {
 })
 
 const updateTarea = asyncHandler(async (req, res) => {
-    res.status(200).json({message:`Modificar la Tarea ${req.params.id}`})
+
+const tarea = await Tarea.findById(req.params.id)
+    if (!tarea){
+        res.status(400)
+        throw new Error('La tarea no fue creada')
+    }
+
+    const tareaUpdated = await Tarea.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(tareaUpdated)
 })
 
 const deleteTarea = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Eliminar la tarea ${req.params.id}`})
+    const tarea = await Tarea.findById(req.params.id)
+    if (!tarea){
+        res.status(400)
+        throw new Error('La tarea no fue creada')
+    }
+    await  tarea.deleteOne()
+
+   //await Tarea.findByIdAndDelete(req.params.id)
+
+    res.status(200).json({id: `Eliminar la tarea ${req.params.id}`})
 })
 
 
